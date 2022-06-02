de873a00353558da8db229e65f48b0fdfef4d7d9
tahamousavi <tahamousavi.sbu@gmail.com>
2022-06-02 12:14:34 +0430

    Models added

--- NodeJS/models/chat.js
+++ NodeJS/models/chat.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const chatSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userID: { type: String},
-    messages:{type: mongoose.Schema.Types.ObjectId, ref: 'MessageSchema' },
+    messages:{type: [mongoose.Schema.Types.ObjectId], ref: 'MessageSchema' },
    
 });
 
